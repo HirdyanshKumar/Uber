ca931d23ab60ebac589d38d0356e004872e21b92
HirdyanshKumar <kumarhirdyansh25@gmail.com>
2025-11-18 01:07:49 +0530

    initial setup

--- backend/app.js
+++ backend/app.js
@@ -3,12 +3,13 @@ dotenv.config()
 const express = require('express');
 const app = express();
 const cors = require('cors')
+const connectToDb = require('./db/db')
+
 
 app.use(cors())
 
-app.get('/',(req,res)=>{
-    res.send("Hello")
-})
+connectToDb()
+
 
 
 module.exports = app;
