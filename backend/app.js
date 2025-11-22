f7f27b597e2ea8caa25445d8ec98692680422685
HirdyanshKumar <kumarhirdyansh25@gmail.com>
2025-11-23 02:42:05 +0530

    user register

--- backend/app.js
+++ backend/app.js
@@ -4,11 +4,18 @@ const express = require('express');
 const app = express();
 const cors = require('cors')
 const connectToDb = require('./db/db')
+const userRoutes = require('./routes/user.routes');
+
+
+connectToDb()
 
 
 app.use(cors())
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 
-connectToDb()
+app.use('/users',userRoutes);
+
 
 
 
--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,4 +1,8 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const dotenv = require('dotenv');
+dotenv.config()
 
 const userSchema = new mongoose.Schema({
     fullname:{
@@ -7,7 +11,7 @@ const userSchema = new mongoose.Schema({
             required: true,
             minlength:[3,'First name must be at least 3 characters long'],
         },
-        lasttname:{
+        lastname:{
             type:String,
             minlength:[3,'Last name must be at least 3 characters long'],
         }
@@ -21,9 +25,29 @@ const userSchema = new mongoose.Schema({
     password:{
         type:String,
         required:true,
+        select : false,
     },
     socketId:{
         type:String,
     },
 
 })
+
+userSchema.methods.generateAuthToken = function (){
+    const token = jwt.sign({_id: this._id},process.env.JWT_SECRET)
+    return token
+}
+
+
+userSchema.methods.comparePassword = async function (password) {
+    return await bcrypt.compare(password,this.password)    
+}
+
+userSchema.statics.hashPassword = async function (password) {
+    return await bcrypt.hash(password,10);    
+}
+
+const userModal = mongoose.model('user',userSchema);
+
+
+module.exports = userModal;
